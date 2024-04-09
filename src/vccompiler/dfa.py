8392a139687eb2bfe355e7eaa204b2abdb8cda9b
Nguyen Viet Dung <29406816+magnified103@users.noreply.github.com>
2024-04-09 14:18:09 +0700

    Initial commit

---
+++ src/vccompiler/dfa.py
@@ -0,0 +1,35 @@
+from graph import State
+
+
+class DFA:
+    def __init__(self, initial_states):
+        self.initial_states = initial_states
+    
+    def search(self, content):
+        end_states = []
+
+        for state in self.initial_states:
+            token = ""
+            
+            for ch in content:
+                state = state.consume(ch)
+                if state is State.none:
+                    break
+                if state.is_end_state():
+                    break
+                token += ch
+
+            if state is State.none:
+                continue
+
+            if state.is_end_state():
+                end_states.append((token, state))
+        
+        if len(end_states) == 0:
+            raise RuntimeError("none state reached")
+
+        if len(end_states) > 1:
+            raise RuntimeError("dfa error: ambiguous state")
+
+        token, state = end_states.pop()
+        return state.callback(token)
