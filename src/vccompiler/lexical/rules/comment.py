edea56c2e618ede8d57646ab9cb4b500bf7140c6
Nguyen Viet Dung <29406816+magnified103@users.noreply.github.com>
2024-04-09 18:00:36 +0700

    Fix comment EOF

--- src/vccompiler/lexical/rules/comment.py
+++ src/vccompiler/lexical/rules/comment.py
@@ -1,30 +1,29 @@
 from ...dfa import EndState, State
 from ..token import TokenEnum
+from .. import charset
 
 
 def comment_cb(token):
     return token, TokenEnum.COMMENT
 
 
-state0 = State(0)
+state0 = State(0)   # begin state
 state1 = State(1)
 state2 = State(2)
-state3 = State(3)
+state3 = EndState(3, comment_cb)
 state4 = State(4)
 state5 = State(5)
 state6 = State(6)
-state7 = EndState(7, comment_cb)    # end state
-state8 = EndState(8, comment_cb)    # end state
+state7 = EndState(7, comment_cb)
 
 state0.add_transition("/", state1)
 state1.add_transition("/", state2)
 state2.set_default_transition(state2)
-state2.add_transition("\n", state3)
-state3.set_default_transition(state7)
+state2.add_transition("\n" + charset.EOF, state3)
 
 state1.add_transition("*", state4)
 state4.set_default_transition(state4)
 state4.add_transition("*", state5)
 state5.set_default_transition(state4)
 state5.add_transition("/", state6)
-state6.set_default_transition(state8)
+state6.set_default_transition(state7)
--- test.py
+++ test.py
@@ -11,18 +11,9 @@ content = ""
 with open(args.input_file, "r") as f:
     content = f.read()
 
-content += '\0'     # EOF
-
-
-# alias = {
-#     "whitespace": " \t\f\r\n",
-#     "newline": "\n",
-#     "letter": string.ascii_letters + "_",
-#     "digit": string.digits,
-# }
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
 from src.vccompiler.lexical.parser import preprocess
 
-print(preprocess(content))
+print(preprocess(content).encode())
