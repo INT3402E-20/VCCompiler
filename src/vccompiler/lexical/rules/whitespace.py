bf323ca9935e6c53b5582f2a0c592332b76aa2c3
Nguyen Viet Dung <29406816+magnified103@users.noreply.github.com>
2024-04-10 01:42:54 +0700

    Rename `add_transition`, `set_default_transition`; remove `add_negative_transition`

--- src/vccompiler/lexical/rules/whitespace.py
+++ src/vccompiler/lexical/rules/whitespace.py
@@ -11,6 +11,6 @@ state0 = State(0)   # begin state
 state1 = State(1)
 state2 = EndState(2, whitespace_cb)
 
-state0.add_transition(alias.whitespace, state1)
-state1.add_transition(alias.whitespace, state1)
-state1.set_default_transition(state2)
+state0.add(alias.whitespace, state1)
+state1.add(alias.whitespace, state1)
+state1.default(state2)
