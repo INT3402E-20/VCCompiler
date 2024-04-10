732cdc5b2675485aa90a60eadac73e8e70809bc2
Nguyen Viet Dung <29406816+magnified103@users.noreply.github.com>
2024-04-10 22:58:58 +0700

    lexer rules: add string literal

--- src/vccompiler/lexer/rules/__init__.py
+++ src/vccompiler/lexer/rules/__init__.py
@@ -1,9 +1,10 @@
 from .comment import state0 as comment
 from .whitespace import state0 as whitespace
+from .string_literal import state0 as string_literal
 from ...dfa import DFA
 
 
 dfa = DFA([
     whitespace,
-
+    string_literal,
 ])
