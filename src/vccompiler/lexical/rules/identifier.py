2a302c777fb7d3c09e5120791b6c327f868fc61d
Hieu Nguyen <hehecoi@outlook.com>
2024-04-10 10:54:48 +0700

    Feat(Identifier): Update identifier base
    Upload identifier graph
    
    UPNEXT: Documenting this feature, solve keyword

--- src/vccompiler/lexical/rules/identifier.py
+++ src/vccompiler/lexical/rules/identifier.py
@@ -1,1 +1,12 @@
-from ...dfa import DFA, State
+from ..token import TokenEnum
+from ...dfa import EndState, State
+from ..charset import alias
+
+def identifier_cb(token):
+    return token, TokenEnum.IDENTIFIER
+
+state0 = State(0)   # begin state
+state1 = EndState(1, identifier_cb)
+
+state0.add_transition(alias["letter"], state1)
+state1.set_default_transition(state1)
