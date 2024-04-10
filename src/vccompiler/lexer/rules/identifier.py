76472aa6c823a8c516f56818b8be457458b2aa95
Hieu Nguyen <hehecoi@outlook.com>
2024-04-11 02:32:45 +0700

    Feat(Key,Iden,BoolLit): Finalizing keyword_identifer tokenizing
    Add tokenize identifier in existed keyword graph. Add boolean literal.
    
    BREAKING CHANGE: Convert normal state to end state in keyword state creation. Might bring unexpected behaviour as well
    TESTING: Until now not found anything breaking, but need more test on this

--- src/vccompiler/lexer/rules/identifier.py
+++ src/vccompiler/lexer/rules/identifier.py
@@ -9,6 +9,6 @@ state0 = State(0)   # begin state
 state1 = State(1)
 state2 = EndState(2, identifier_cb)
 
-state0.add(alias["letter"], state1)
-state1.add(alias["letter"] + alias["digit"], state1)
+state0.add(alias.letter, state1)
+state1.add(alias.letter + alias.digit, state1)
 state1.default(state2)
