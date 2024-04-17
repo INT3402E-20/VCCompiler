59adb76c08ac0271add2d3aeab8a494fffce8a57
Nguyen Viet Dung <29406816+magnified103@users.noreply.github.com>
2024-04-17 12:23:59 +0700

    Set `insert_keyword` as a method of `State`

--- src/vccompiler/lexer/rules/vc/identifier.py
+++ src/vccompiler/lexer/rules/vc/identifier.py
@@ -1,7 +1,6 @@
 from vccompiler.dfa import State
 from vccompiler.lexer.token_types import TokenEnum
 from vccompiler.lexer.charset import *
-from vccompiler.lexer.util import insert_keyword
 
 
 state0 = State(0)
@@ -14,7 +13,7 @@ KEYWORDS = ["boolean", "break", "continue", "else", "float", "for", "if", "int",
 LITERALS = ["true", "false"]
 
 for keyword in KEYWORDS:
-    insert_keyword(state0, keyword, -1, TokenEnum.KEYWORD)
+    state0.insert_keyword(keyword, -1, TokenEnum.KEYWORD)
 
 for literal in LITERALS:
-    insert_keyword(state0, literal, -1, TokenEnum.LITERAL)
+    state0.insert_keyword(literal, -1, TokenEnum.LITERAL)
