e23c73c2c717b6d887c24e9a75978af95af1c66f
Hieu Nguyen <hehecoi@outlook.com>
2024-05-15 13:08:55 +0700

    Refactor(Grammar): Refactor to use simple import
    Refactor a bit and divide each sections for clarification
    
    BREAKING CHANGE: GRAMMAR: Change import from long kind Symbol, ... to short one S, F, L,...

--- src/vccompiler/parser/grammars/vc.py
+++ src/vccompiler/parser/grammars/vc.py
@@ -1,95 +1,121 @@
 from vccompiler.lexer.token import TokenEnum
-from vccompiler.ll1 import Format, LL1Grammar, Symbol
+from vccompiler.ll1 import Format as F, LL1Grammar as L, Symbol as S
 
 
-program = Symbol("program")   # start symbol
-declare_type = Symbol("declare-type")
-vc_type = Symbol("type", TokenEnum.TYPE_INIT)
-identifier = Symbol("identifier", TokenEnum.IDENTIFIER)
+program = S("program")   # start symbol
+declare_type = S("declare-type")
+vc_type = S("type", TokenEnum.TYPE_INIT)
+identifier = S("identifier", TokenEnum.IDENTIFIER)
 
-func_decl = Symbol("func-decl")
-para_list = Symbol("para-list")
-compound_stmt = Symbol("compound-stmt")
-proper_para_list = Symbol("proper-para-list")
-temp_para_list = Symbol("temp-para-list")
-para_decl = Symbol("para-decl")
-array_decl = Symbol("array-decl")
-array_size = Symbol("array-size")
+func_decl = S("func-decl")
+para_list = S("para-list")
+compound_stmt = S("compound-stmt")
+proper_para_list = S("proper-para-list")
+temp_para_list = S("temp-para-list")
+para_decl = S("para-decl")
+array_decl = S("array-decl")
+array_size = S("array-size")
 
-temp_compound = Symbol("temp-compound")
-var_decl = Symbol("var-decl")
-temp_compound_next = Symbol("temp-compound-next")
-init_decl_tial = Symbol("init-decl-tial")
-temp_init_decl = Symbol("temp-init-decl")
-init_decl = Symbol("init-decl")
-initialiser = Symbol("initialiser")
-many_expr = Symbol("many-expr")
-expr = Symbol("expr")
+temp_compound = S("temp-compound")
+var_decl = S("var-decl")
+temp_compound_next = S("temp-compound-next")
+init_decl_tial = S("init-decl-tial")
+temp_init_decl = S("temp-init-decl")
+init_decl = S("init-decl")
+initialiser = S("initialiser")
+many_expr = S("many-expr")
+expr = S("expr")
 
-stmt = Symbol("stmt")
-if_stmt = Symbol("if-stmt")
-for_stmt = Symbol("for-stmt")
-while_stmt = Symbol("while-stmt")
-break_stmt = Symbol("break-stmt")
-continue_stmt = Symbol("continue-stmt")
-return_stmt = Symbol("return-stmt")
-expr_stmt = Symbol("expr-stmt")
-is_expr = Symbol("is-expr")
+stmt = S("stmt")
+if_stmt = S("if-stmt")
+for_stmt = S("for-stmt")
+while_stmt = S("while-stmt")
+break_stmt = S("break-stmt")
+continue_stmt = S("continue-stmt")
+return_stmt = S("return-stmt")
+expr_stmt = S("expr-stmt")
+is_expr = S("is-expr")
 
-grammar = LL1Grammar(program)
+grammar = L(program)
 # grammar.add_rule(program, func_decl, program)
 
-grammar.add_rule(program, vc_type, identifier, declare_type, program)
-grammar.add_rule(program, Symbol.eps)
-grammar.add_rule(declare_type, func_decl)
-grammar.add_rule(declare_type, var_decl)
+program_rules = [
+    (program, vc_type, identifier, declare_type, program),
+    (program, S.eps),
+    (declare_type, func_decl),
+    (declare_type, var_decl),
+]
 
-grammar.add_rule(func_decl, para_list, compound_stmt)
-grammar.add_rule(para_list, "(", proper_para_list, ")")
-grammar.add_rule(proper_para_list, para_decl, temp_para_list)
-grammar.add_rule(proper_para_list, Symbol.eps)
-grammar.add_rule(temp_para_list, ",", para_decl, temp_para_list)
-grammar.add_rule(temp_para_list, Symbol.eps)
-grammar.add_rule(para_decl, vc_type, identifier, array_decl)
-grammar.add_rule(array_decl, "[", array_size, "]")
-grammar.add_rule(array_decl, Symbol.eps)
-grammar.add_rule(array_size, TokenEnum.INTLITERAL)
-grammar.add_rule(array_size, Symbol.eps)
+funtion_rules = [
+    (func_decl, para_list, compound_stmt),
+    (para_list, "(", proper_para_list, ")"),
+    (proper_para_list, para_decl, temp_para_list),
+    (proper_para_list, S.eps),
+    (temp_para_list, ",", para_decl, temp_para_list),
+    (temp_para_list, S.eps),
+    (para_decl, vc_type, identifier, array_decl),
+]
 
-grammar.add_rule(compound_stmt, "{" , temp_compound_next, "}")
-grammar.add_rule(temp_compound, vc_type, var_decl, temp_compound_next)
-grammar.add_rule(temp_compound, stmt, temp_compound_next)
-grammar.add_rule(temp_compound_next, temp_compound)
-grammar.add_rule(temp_compound_next, Symbol.eps)
+declaration_rules = [
+    (var_decl, array_decl, init_decl_tial, temp_init_decl, ";"),
+    (temp_init_decl, ",", init_decl, temp_init_decl),
+    (temp_init_decl, S.eps),
+    (init_decl, identifier, array_decl, init_decl_tial),
+    (init_decl_tial, "=", initialiser),
+    (init_decl_tial, S.eps),
+    (initialiser, expr),
+    (initialiser, "{", expr, many_expr, "}"),
+    (many_expr, ",", expr, many_expr),
+    (many_expr, S.eps),
+    (array_decl, "[", array_size, "]"),
+    (array_decl, S.eps),
+    (array_size, TokenEnum.INTLITERAL),
+    (array_size, S.eps),
+]
 
-grammar.add_rule(var_decl, array_decl, init_decl_tial, temp_init_decl, ";")
-grammar.add_rule(temp_init_decl, ",", init_decl, temp_init_decl)
-grammar.add_rule(temp_init_decl, Symbol.eps)
-grammar.add_rule(init_decl, identifier, array_decl, init_decl_tial)
-grammar.add_rule(init_decl_tial, "=", initialiser)
-grammar.add_rule(init_decl_tial, Symbol.eps)
-grammar.add_rule(initialiser, expr)
-grammar.add_rule(initialiser, "{", expr, many_expr, "}")
-grammar.add_rule(many_expr, ",", expr, many_expr)
-grammar.add_rule(many_expr, Symbol.eps)
+statement_rules = [
+    (stmt, compound_stmt),
+    (stmt, if_stmt),
+    (stmt, for_stmt),
+    (stmt, while_stmt),
+    (stmt, break_stmt),
+    (stmt, continue_stmt),
+    (stmt, return_stmt),
+    (stmt, expr_stmt),
+]
 
-grammar.add_rule(stmt, compound_stmt)
-grammar.add_rule(stmt, if_stmt)
-grammar.add_rule(stmt, for_stmt)
-grammar.add_rule(stmt, while_stmt)
-grammar.add_rule(stmt, break_stmt)
-grammar.add_rule(stmt, continue_stmt)
-grammar.add_rule(stmt, return_stmt)
-grammar.add_rule(stmt, expr_stmt)
+compound_stmt_rules = [
+    (compound_stmt, "{" , temp_compound_next, "}"),
+    (temp_compound, vc_type, var_decl, temp_compound_next),
+    (temp_compound, stmt, temp_compound_next),
+    (temp_compound_next, temp_compound),
+    (temp_compound_next, S.eps),
+]
 
-grammar.add_rule(if_stmt, "if", "(", expr, ")", stmt, "else", stmt)
-grammar.add_rule(for_stmt, "for", "(", is_expr, ";", is_expr, ";", is_expr, ")", stmt)
-grammar.add_rule(is_expr, expr)
-grammar.add_rule(is_expr, Symbol.eps)
-grammar.add_rule(while_stmt, "while", "(", expr, ")", stmt)
-grammar.add_rule(break_stmt, "break", ";")
-grammar.add_rule(continue_stmt, "continue", ";")
-grammar.add_rule(return_stmt, "return", is_expr, ";")
-grammar.add_rule(expr_stmt, is_expr, ";")
+other_stmt_rules = [
+    (if_stmt, "if", "(", expr, ")", stmt, "else", stmt),
+    (for_stmt, "for", "(", is_expr, ";", is_expr, ";", is_expr, ")", stmt),
+    (is_expr, expr),
+    (is_expr, S.eps),
+    (while_stmt, "while", "(", expr, ")", stmt),
+    (break_stmt, "break", ";"),
+    (continue_stmt, "continue", ";"),
+    (return_stmt, "return", is_expr, ";"),
+    (expr_stmt, is_expr, ";"),
+]
 
 # Continue with expr rule
+
+for rule in program_rules:
+    grammar.add_rule(*rule)
+for rule in funtion_rules:
+    grammar.add_rule(*rule)
+for rule in declaration_rules:
+    grammar.add_rule(*rule)
+for rule in statement_rules:
+    grammar.add_rule(*rule)
+for rule in compound_stmt_rules:
+    grammar.add_rule(*rule)
+for rule in other_stmt_rules:
+    grammar.add_rule(*rule)
+    
